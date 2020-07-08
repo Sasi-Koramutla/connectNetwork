1d3eb231a8cee4827020bacb52ca5699844a9f76
Sasi Koramutla <sasidhar.kmv@gmail.com>
2020-07-08 17:05:51 -0400

    setup the directory and packages

---
+++ server.js
@@ -0,0 +1,52 @@
+//express, mongoose, methodOverride, session
+const express = require("express");
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const session = require('express-session');
+
+//dotenv, port, URI
+require('dotenv').config();
+const PORT = process.env.PORT;
+const mongodbURI = process.env.MONGODBURI;
+
+//// middleware to help with the form submission
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
+//session usage
+app.use(
+    session({
+      secret: process.env.SECRET, //a random string do not copy this value or your stuff will get hacked
+      resave: false, // default more info: https://www.npmjs.com/package/express-session#resave
+      saveUninitialized: false // default  more info: https://www.npmjs.com/package/express-session#resave
+    })
+  );
+  
+// mongoose connection logic
+  mongoose.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true } );
+  mongoose.connection.once('open', ()=> {
+      console.log('connected to mongo');
+  });
+
+
+//Connections controller
+const connectionsController = require("./controllers/connections.js");
+app.use("/connections", connectionsController);
+
+// User controller
+const userController = require('./controllers/users.js');
+app.use('/users', userController);
+
+// Session controller
+const sessionController = require('./controllers/sessions.js');
+app.use('/sessions', sessionController);
+
+
+//get route
+app.get("/", (req, res) => {
+    res.send("At the root!");
+});
+// the app running the server
+app.listen(PORT, () => {
+    console.log('listening')
+  });
